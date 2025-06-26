f60eb77906183200d4e1e359e6e8e11052de8fcc
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-06-26 22:11:12 +0800

    added_widgets

--- mp3_player_oop_style.py
+++ mp3_player_oop_style.py
@@ -5,10 +5,13 @@ from tkinter import *
 
 class mp3_player_launcher:
     def __init__(self):
-        self.imports = imports()
         self.root = tk.TK()
         self.root.title("MP3 Player Launcher")
         self.root.geometry("300x200")
         self.create_widgets()
         self.root.mainloop()
-root = tk.TK()
+
+class widget:
+    def __init__(self, master):
+        self.master = master
+        self.label = tk.Label(master, text = "mp3 player launcher")
