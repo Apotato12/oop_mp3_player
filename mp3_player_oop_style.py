4f74b35564cd4f71fc8aa001214a5d499eb32011
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-06-26 21:53:50 +0800

    added_the_framework

--- mp3_player_oop_style.py
+++ mp3_player_oop_style.py
@@ -1,3 +1,14 @@
+import tkinter as tk
 import pygame
 import os
-import tkinter as tk
+from tkinter import *
+
+class mp3_player_launcher:
+    def __init__(self):
+        self.imports = imports()
+        self.root = tk.TK()
+        self.root.title("MP3 Player Launcher")
+        self.root.geometry("300x200")
+        self.create_widgets()
+        self.root.mainloop()
+root = tk.TK()
