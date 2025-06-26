42290942ee7439dcce1e49a707a5451688b37bba
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-06-26 22:29:14 +0800

    added_controls

--- Widgets/widgets.py
+++ Widgets/widgets.py
@@ -2,5 +2,27 @@ from mp3_player_oop_style import pygame , tkinter as tk  , os
 from tkinter import ttk, filedialog, messagebox
 from pygame import mixer
 
+class PlayerControls(tk.Frame):
+    def __init__(self, master, player, **kwargs):
+        super().__init__(master, **kwargs)
+        self.player = player
+        
+        self.play_btn = tk.Button(self, text="▶", command=self.player.play_pause)
+        self.play_btn.grid(row=0, column=0, padx=5)
+        
+        self.stop_btn = tk.Button(self, text="⏹", command=self.player.stop)
+        self.stop_btn.grid(row=0, column=1, padx=5)
+
+        self.prev_btn = tk.Button(self, text="⏮", command=self.player.prev_song)
+        self.prev_btn.grid(row=0, column=2, padx=5)
+
+        self.next_btn = tk.Button(self, text="⏭", command=self.player.next_song)
+        self.next_btn.grid(row=0, column=3, padx=5)
+
+        self.volume_scale = tk.Scale(self, from_=0, to=100, orient='horizontal',
+                                   command=self.player.set_volume)
+        self.volume_scale.set(70)
+        self.volume_scale.grid(row=0, column=4, padx=5)
+
 
 
