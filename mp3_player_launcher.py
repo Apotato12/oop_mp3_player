55ecc674efe6af2ed6a149fcc7baeb1fba82aaa1
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-06-28 00:59:22 +0800

    added_modifications_to_the_code_to_improve_functionality

--- mp3_player_launcher.py
+++ mp3_player_launcher.py
@@ -8,12 +8,16 @@ class MP3PlayerApp:
         self.root.title("MP3 Player OOP Style")
         self.root.geometry("600x500")
         self.player = MP3PlayerCore()
-
         self.create_widgets()
         self.player.load_songs(self.player.music_directory)
-        self.update_playlist()
-        self.update_now_playing()
-        self._is_playing = False
+        
+        if self.player.playlist:
+            self.update_playlist()
+            self.player.current_index = 0 
+            self.update_now_playing()  # Ensure this is called only if there are songs
+            self._is_playing = False
+            
+        print("Loaded songs:", self.player.playlist)
         
     def create_widgets(self):
         """Create and arrange all widgets"""
@@ -48,20 +52,22 @@ class MP3PlayerApp:
             self.update_now_playing()
     
     def update_now_playing(self):
-        """Update the 'now playing' label with the current song"""
-        if self.player.playlist and self.player.current_index < len(self.player.playlist):
-            current_song = self.player.playlist[self.player.current_index]
-            self.now_playing_label.config(text=f"Now Playing: {current_song}")
-        else:
-            self.now_playing_label.config(text="Now Playing: No song selected")
+       if self.player.playlist:
+           current_song = self.player.playlist[self.player.current_index]
+           self.now_playing_label.config(text=f"Now Playing: {current_song}")
+       else:
+           self.now_playing_label.config(text="Now Playing: No song selected")
 
     def update_progress_bar(self):
-        if self.player.is_playing:  # Note the change
-         position = self.player.get_position()  
-         duration = self.player.get_duration()  
-         self.controls.progress_bar.set(position / duration * 100)  
-         self.controls.time_label.config(text=f"{position // 60}:{position % 60:02d} / {duration // 60}:{duration % 60:02d}")
-         self.root.after(1000, self.update_progress_bar) 
+        if self.player.is_playing and self.player.current_song:  # Ensure a song is selected
+            position = self.player.get_position()
+            duration = self.player.get_duration()
+            
+            # Prevent division by zero
+            if duration > 0:
+                self.controls.progress_bar.set(position / duration * 100)
+                self.controls.time_label.config(text=f"{position // 60}:{position % 60:02d} / {duration // 60}:{duration % 60:02d}")
+        self.root.after(1000, self.update_progress_bar) 
 
 
     # Methods that connect widgets to core functionality
@@ -83,15 +89,15 @@ class MP3PlayerApp:
 
     def set_volume(self, value):
         self.player.set_volume(value)
-
+        
     def play_selected(self):
         if not self.player.playlist:
            return
         selection = self.playlist_display.listbox.curselection()
         if selection:
             self.player.current_index = selection[0]
-            self.update_now_playing()
             self.player.play_song(self.player.playlist[self.player.current_index])
+            self.update_now_playing()
 
     def load_songs_dialog(self):
        """Open a dialog to load new songs into the playlist"""
